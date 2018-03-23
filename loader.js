c27aebc6fbf9fc0085d6b9857ef85c3a3faaf003
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-03-23 00:59:32 -0300

    update server

--- loader.js
+++ loader.js
@@ -1,9 +1,9 @@
 // const server = require('./config/server');
-const ServerClass = require('./config-soap/server');
+const ServerClass = require('./config-oo/server');
 const RoutesClass = require('./config/routes');
 
-let server = new ServerClass(4000);
+const server = new ServerClass(4000);
 
 require('./config/database');
-let routes = new RoutesClass(server.getServer());
+const routes = new RoutesClass(server.getServer());
 // require('./config/routes')(server.getServer());
